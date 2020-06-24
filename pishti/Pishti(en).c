e69a19d8741409ec43761b67f052c5ed7944b48f
Baran Aytaş <38444159+baranaytass@users.noreply.github.com>
2020-06-24 15:25:32 +0300

    Add files via upload

---
+++ pishti/Pishti(en).c
@@ -0,0 +1,357 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/*
+deste için bir integer dizisi yaratip her bir elemana 3 basamakli bir sayi atayacagim.
+bu sayilarin yüzler basamagi kartin cinsini belirtecek.
+Kupa>Karo>Maça>Sinek   >>>>  400>300>200>100
+As>Papaz>Kiz>Vale   >>>> 14>13>12>11
+
+Vize ödevinde kullandigim deste yapisini(structure'dan ayirarak) ayni sekilde kullandim. Diziler artik structlarin içinde degil de pointer mantigiyla
+çalisiyor.
+
+Bilgisayar kart atarken belli bir mantikla atiyor karti. Ancak isin sansa kaldigi durumlarda (yerdekileri toplayamiyorken vs.)random
+fonksiyonunu kullaniyor. -> chooseComCard()
+
+Taraflardan ikisi için de kartlari toplayacagi zaman kullanilacak olan captureCards adinda bir fonksiyon kullandim. Ancak bu fonksiyon bir
+tepki verecegi zaman(pisti yapildiginda skoru arttirmak vs. gibi) hangi oyuncu tarafindan yapildigini anlamasi için static göstericiler(*ptrA/*ptrB)
+kullandim bu göstericiyi de captureCards fonksiyonundaki parametrelerden biri olan player1/player2 dizilerini gösteriyor. Bu diziler taraflarin
+topladigi kartlari tutan diziler.
+
+*/
+
+static int lastCapture=0;
+static int *ptrA;
+static int scoreA=0;
+static int *ptrB;
+static int scoreB=0;
+
+int deal(int *deck, int *hand){
+    int x=0;
+    for(int i=0;i<52;i++){
+        if(deck[i]!=0){
+            for(int j=i;j<i+4;j++){
+                hand[x++]=deck[j];
+                deck[j]=0;
+            }
+            return 0;
+        }
+    }
+    return 0;
+}
+
+void createDeck(int *deck){
+    for(int i=0;i<52;i++){
+        deck[i]=(100*((i/13)+1))+(i%13)+2;
+    }
+}
+
+void createEmpityDeck(int *deck){
+    for(int i=0;i<52;i++)
+        deck[i]=0;
+}
+
+void shuffleDeck(int *deck){
+    srand(time(NULL));
+    int temp;
+    for(int i=0;i<52;i++){
+        int random1 = rand()%(52-i);
+        int random2 = rand()%(52-i);
+        temp=deck[random1];
+        deck[random1]=deck[random2];
+        deck[random2]=temp;
+    }
+}
+
+void printCard(int x){
+    if(x==0)
+        printf("Empity   ");
+    if(x/100==1)
+        printf("Clubs ");
+    else if(x/100==2)
+        printf("Spades ");
+    else if(x/100==3)
+        printf("Diamonds ");
+    else if(x/100==4)
+        printf("Hearts ");
+    if(x%100==14)
+        printf("Ace   ");
+    else if(x%100==13)
+        printf("King   ");
+    else if(x%100==12)
+        printf("Queen   ");
+    else if(x%100==11)
+        printf("Jack   ");
+    else{
+        if(x!=0)
+            printf("%d   ",x%100);
+    }
+}
+
+void printCards(int *deck, int length){
+    for(int i=0;i<length;i++){
+        if(deck[i]!=0){
+            printCard(deck[i]);
+            printf("-   ");
+        }
+    }
+}
+
+int getTop(int *deck){
+    if(deck[0]==0){
+        return 0;
+    }
+    for(int i=0;i<52;i++){
+        if(deck[i]==0){
+            return deck[i-1];
+        }
+    }
+}
+
+void captureRemainedCards(int *player, int *ground){
+    if(player==ptrA)
+        printf("\nPlayer Captured The Remaining Cards!!");
+    if(player==ptrB)
+        printf("\nComputer Captured The Remaining Cards!!");
+
+    int t=0;
+    while(1==1){
+        if(player[t]==0)
+            break;
+        t++;
+    }
+    int i=0;
+    while(ground[i]!=0)
+        player[t++]=ground[i++];
+    createEmpityDeck(ground);
+}
+
+void captureCards(int *player, int *ground){
+    if(player==ptrA && howManyCard(ground)==2 && (getTop(ground))%100!=11){
+        printf("\nPlayer Got a Pishti!!\n");
+        scoreA=scoreA+10;
+        lastCapture=0;
+    }
+    if(player==ptrB && howManyCard(ground)==2 && (getTop(ground))%100!=11){
+        printf("\nComputer Got a Pishti!!\n");
+        scoreB=scoreB+10;
+        lastCapture=1;
+    }
+    if(player==ptrA && howManyCard(ground)==2 && (getTop(ground))%100==11 && (ground[0])%100==11){
+        printf("\nPlayer Got a Pishti With Jack!!\n");
+        scoreA=scoreA+20;
+        lastCapture=0;
+    }
+    if(player==ptrB && howManyCard(ground)==2 && (getTop(ground))%100==11 && (ground[0])%100==11){
+        printCard(getTop(ground));
+        printf("\nComputer Got a Pishti With Jack!!\n");
+        scoreA=scoreA+20;
+        lastCapture=1;
+    }
+    if(player==ptrA && howManyCard(ground)==2 && (getTop(ground))%100==11){
+        printf("\nPlayer Captured Cards!!\n");
+        lastCapture=0;
+    }
+    if(player==ptrB && howManyCard(ground)==2 && (getTop(ground))%100==11){
+        printCard(getTop(ground));
+        printf("\nComputer Captured Cards!!\n");
+        lastCapture=1;
+    }
+    if(player==ptrA && howManyCard(ground)>2){
+        printf("\nPlayer Captured Cards!!\n");
+        lastCapture=0;
+    }
+    if(player==ptrB && howManyCard(ground)>2){
+        printCard(getTop(ground));
+        printf("\nComputer Captured Cards!!\n");
+        lastCapture=1;
+    }
+    int t=0;
+    while(1==1){
+        if(player[t]==0)
+            break;
+        t++;
+    }
+    int i=0;
+    while(ground[i]!=0)
+        player[t++]=ground[i++];
+    createEmpityDeck(ground);
+
+}
+
+void playCard(int *hand,int t,int *ground,int *player){
+    int x = getTop(ground);
+    for(int i=0;i<52;i++){
+        if(ground[i]==0){
+            ground[i]=hand[t];
+            hand[t]=0;
+            break;
+        }
+    }
+    if(x%100==(getTop(ground))%100)
+        captureCards(player,ground);
+    if((getTop(ground))%100==11 && howManyCard(ground)>1)
+        captureCards(player,ground);
+
+}
+
+int howManyCard(int *deck){
+    int i=0;
+    while(1){
+        if(deck[i]==0)
+            break;
+        i++;
+    }
+    return i;
+}
+
+int chooseComCard(int *hand, int *ground, int *player){
+    srand(time(NULL));
+    for(int i=0;i<4;i++){
+        if((getTop(ground))%100==(hand[i])%100 && hand[i]!=0){
+            playCard(hand,i,ground,player);
+            return 0;
+        }
+    }
+    for(int i=0;i<4;i++){
+        if((hand[i])%100==11){
+            if(howManyCard(ground)>=3){
+                playCard(hand,i,ground,player);
+                return 0;
+            }
+        }
+    }
+    for(int i=0;i<4;i++){
+        if((hand[i])%100!=11 && hand[i]!=0){
+            while(1){
+                int random2 = rand()%4;
+                if((hand[random2])%100!=11 && hand[random2]!=0){
+                    playCard(hand,random2,ground,player);
+                    return 0;
+                }
+            }
+        }
+    }
+    while(1){
+        int random1 = rand()%4;
+        if(hand[random1]!=0){
+            playCard(hand,random1,ground,player);
+            break;
+        }
+    }
+}
+
+void chooseCard(int *hand,int *ground, int *player){
+    while(1==1){
+        int num;
+        printf("\n\nInput The Card You Want to Discard. :");
+        scanf("%d",&num);
+        switch (num) {
+            case 1:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nInvalid Choice!\n");
+                break;
+            case 2:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nInvalid Choice!\n");
+                break;
+            case 3:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nInvalid Choice!\n");
+                break;
+            case 4:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nInvalid Choice!\n");
+                break;
+        }
+    }
+}
+
+void scoreCalculator(int *player1,int *player2){
+    if(howManyCard(player1)>howManyCard(player2))
+        scoreA=scoreA+3;
+    if(howManyCard(player1)<howManyCard(player2))
+        scoreB=scoreB+3;
+    for(int i=0;i<52;i++){
+        if(player1[i]==202)
+            scoreA=scoreA+2;
+        if(player1[i]==310)
+            scoreA=scoreA+3;
+        if((player1[i])%100==14 || (player1[i])%100==11)
+            scoreA=scoreA+1;
+
+        if(player2[i]==202)
+            scoreB=scoreB+2;
+        if(player2[i]==310)
+            scoreB=scoreB+3;
+        if((player2[i])%100==14 || (player2[i])%100==11)
+            scoreB=scoreB+1;
+    }
+}
+
+int main(){
+    int deck1[52]; createDeck(deck1); shuffleDeck(deck1);
+    int player1[52]; createEmpityDeck(player1);
+    int hand1[4];
+    int player2[52]; createEmpityDeck(player2);
+    int hand2[4];
+    int ground[52]; createEmpityDeck(ground);
+
+    ptrA=player1;
+    ptrB=player2;
+
+    deal(deck1,ground);
+
+    for(int turn=1;turn<7;turn++){
+        if(turn==1)
+            printf("------ GAME ON! ------");
+        printf("\n\n\n-- %d. TURN --\n",turn);
+        printf("\nThe Top Card on The Ground  : ");
+        printCard(getTop(ground));
+        deal(deck1,hand1);
+        deal(deck1,hand2);
+
+        for(int i=0;i<4;i++){
+            printf("\n\nThe Cards in Your Hand  :   ");
+            for(int i=0;i<4;i++){
+                printf("%d- ",i+1);
+                printCard(hand1[i]);
+            }
+            chooseCard(hand1,ground,player1);
+            printf("\nThe Top Card on The Ground :");
+            printCard(getTop(ground));
+            chooseComCard(hand2,ground,player2);
+            printf("\n\nThe Top Card on The Ground :");
+            printCard(getTop(ground));
+        }
+        if(turn==6){
+            if(lastCapture==0)
+                captureRemainedCards(player1, ground);
+            if(lastCapture==1)
+                captureRemainedCards(player2, ground);
+        }
+        printf("\n\nThe Cards Captured by The Player  : ");
+        printCards(player1,52);
+        printf("\n\nThe Cards Captured by The Computer  : ");
+        printCards(player2,52);
+    }
+
+    scoreCalculator(player1,player2);
+    printf("\nPLAYER'S SCORE  : %d",scoreA);
+    printf("\nCOMPUTER'S SCORE  : %d",scoreB);
+    if(scoreA>scoreB)
+        printf("\n---  PLAYER WON  ---");
+    else if(scoreA>scoreB)
+        printf("\n---  COMPUTER WON  ---");
+    else
+        printf("\n---  DRAW  ---");
+}
---
+++ pishti/Pishti(tr).c
@@ -0,0 +1,343 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/*
+deste için bir integer dizisi yaratip her bir elemana 3 basamakli bir sayi atayacagim.
+bu sayilarin yüzler basamagi kartin cinsini belirtecek.
+Kupa>Karo>Maça>Sinek   >>>>  400>300>200>100
+As>Papaz>Kiz>Vale   >>>> 14>13>12>11
+
+*/
+
+static int lastCaptured=0;
+static int *ptrA;
+static int scoreA=0;
+static int *ptrB;
+static int scoreB=0;
+
+int deal(int *deck, int *hand){
+    int x=0;
+    for(int i=0;i<52;i++){
+        if(deck[i]!=0){
+            for(int j=i;j<i+4;j++){
+                hand[x++]=deck[j];
+                deck[j]=0;
+            }
+            return 0;
+        }
+    }
+    return 0;
+}
+
+void createDeck(int *deck){
+    for(int i=0;i<52;i++){
+        deck[i]=(100*((i/13)+1))+(i%13)+2;
+    }
+}
+
+void createEmpityDeck(int *deck){
+    for(int i=0;i<52;i++)
+        deck[i]=0;
+}
+
+void shuffleDeck(int *deck){
+    srand(time(NULL));
+    int temp;
+    for(int i=0;i<52;i++){
+        int random1 = rand()%(52-i);
+        int random2 = rand()%(52-i);
+        temp=deck[random1];
+        deck[random1]=deck[random2];
+        deck[random2]=temp;
+    }
+}
+
+void printCard(int x){
+    if(x==0)
+        printf("Yok   ");
+    if(x/100==1)
+        printf("Sinek ");
+    else if(x/100==2)
+        printf("Maca ");
+    else if(x/100==3)
+        printf("Karo ");
+    else if(x/100==4)
+        printf("Kupa ");
+    if(x%100==14)
+        printf("As   ");
+    else if(x%100==13)
+        printf("Papaz   ");
+    else if(x%100==12)
+        printf("Kiz   ");
+    else if(x%100==11)
+        printf("Vale   ");
+    else{
+        if(x!=0)
+            printf("%d   ",x%100);
+    }
+}
+
+void printCards(int *deck, int length){
+    for(int i=0;i<length;i++){
+        if(deck[i]!=0)
+            printCard(deck[i]);
+    }
+}
+
+int getTop(int *deck){
+    if(deck[0]==0){
+        return 0;
+    }
+    for(int i=0;i<52;i++){
+        if(deck[i]==0){
+            return deck[i-1];
+        }
+    }
+}
+
+void captureRemainedCards(int *player, int *ground){
+    if(player==ptrA)
+        printf("\nOyuncu Kalan Kartlari Topladi");
+    if(player==ptrB)
+        printf("\nBilgisayar Kalan Kartlari Topladi");
+
+    int t=0;
+    while(1==1){
+        if(player[t]==0)
+            break;
+        t++;
+    }
+    int i=0;
+    while(ground[i]!=0)
+        player[t++]=ground[i++];
+    createEmpityDeck(ground);
+}
+
+void captureCards(int *player, int *ground){
+    if(player==ptrA && howManyCard(ground)==2 && (getTop(ground))%100!=11){
+        printf("\nOyuncu Pisti Yapti!!\n");
+        scoreA=scoreA+10;
+        lastCaptured=0;
+    }
+    if(player==ptrB && howManyCard(ground)==2 && (getTop(ground))%100!=11){
+        printf("\nBilgisayar Pisti Yapti!!\n");
+        scoreB=scoreB+10;
+        lastCaptured=1;
+    }
+    if(player==ptrA && howManyCard(ground)==2 && (getTop(ground))%100==11 && (ground[0])%100==11){
+        printf("\nOyuncu Valeyle Pisti Yapti!!\n");
+        scoreA=scoreA+20;
+        lastCaptured=0;
+    }
+    if(player==ptrB && howManyCard(ground)==2 && (getTop(ground))%100==11 && (ground[0])%100==11){
+        printCard(getTop(ground));
+        printf("\nBilgisayar Valeyle Pisti Yapti!!\n");
+        scoreA=scoreA+20;
+        lastCaptured=1;
+    }
+    if(player==ptrA && howManyCard(ground)==2 && (getTop(ground))%100==11){
+        printf("\nOyuncu Kartlari Topladi!!\n");
+        lastCaptured=0;
+    }
+    if(player==ptrB && howManyCard(ground)==2 && (getTop(ground))%100==11){
+        printCard(getTop(ground));
+        printf("\nBilgisayar Kartlari Topladi!!\n");
+        lastCaptured=1;
+    }
+    if(player==ptrA && howManyCard(ground)>2){
+        printf("\nOyuncu Kartlari Topladi!!\n");
+        lastCaptured=0;
+    }
+    if(player==ptrB && howManyCard(ground)>2){
+        printCard(getTop(ground));
+        printf("\nBilgisayar Kartlari Topladi!!\n");
+        lastCaptured=1;
+    }
+    int t=0;
+    while(1==1){
+        if(player[t]==0)
+            break;
+        t++;
+    }
+    int i=0;
+    while(ground[i]!=0)
+        player[t++]=ground[i++];
+    createEmpityDeck(ground);
+
+}
+
+void playCard(int *hand,int t,int *ground,int *player){
+    int x = getTop(ground);
+    for(int i=0;i<52;i++){
+        if(ground[i]==0){
+            ground[i]=hand[t];
+            hand[t]=0;
+            break;
+        }
+    }
+    if(x%100==(getTop(ground))%100)
+        captureCards(player,ground);
+    if((getTop(ground))%100==11 && howManyCard(ground)>1)
+        captureCards(player,ground);
+
+}
+
+int howManyCard(int *deck){
+    int i=0;
+    while(1){
+        if(deck[i]==0)
+            break;
+        i++;
+    }
+    return i;
+}
+
+int chooseComCard(int *hand, int *ground, int *player){
+    srand(time(NULL));
+    for(int i=0;i<4;i++){
+        if((getTop(ground))%100==(hand[i])%100 && hand[i]!=0){
+            playCard(hand,i,ground,player);
+            return 0;
+        }
+    }
+    for(int i=0;i<4;i++){
+        if((hand[i])%100==11){
+            if(howManyCard(ground)>=3){
+                playCard(hand,i,ground,player);
+                return 0;
+            }
+        }
+    }
+    for(int i=0;i<4;i++){
+        if((hand[i])%100!=11 && hand[i]!=0){
+            while(1){
+                int random2 = rand()%4;
+                if((hand[random2])%100!=11 && hand[random2]!=0){
+                    playCard(hand,random2,ground,player);
+                    return 0;
+                }
+            }
+        }
+    }
+    while(1){
+        int random1 = rand()%4;
+        if(hand[random1]!=0){
+            playCard(hand,random1,ground,player);
+            break;
+        }
+    }
+}
+
+void chooseCard(int *hand,int *ground, int *player){
+    while(1==1){
+        int num;
+        printf("\n\nAtmak istediginiz Kartin numarasini girin :");
+        scanf("%d",&num);
+        switch (num) {
+            case 1:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nGecersiz Kart!\n");
+                break;
+            case 2:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nGecersiz Kart!\n");
+                break;
+            case 3:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nGecersiz Kart!\n");
+                break;
+            case 4:
+                if(hand[num-1]!=0)
+                    return playCard(hand,num-1,ground,player);
+                else
+                    printf("\nGecersiz Kart!\n");
+                break;
+        }
+    }
+}
+
+void scoreCalculator(int *player1,int *player2){
+    if(howManyCard(player1)>howManyCard(player2))
+        scoreA=scoreA+3;
+    if(howManyCard(player1)<howManyCard(player2))
+        scoreB=scoreB+3;
+    for(int i=0;i<52;i++){
+        if(player1[i]==202)
+            scoreA=scoreA+2;
+        if(player1[i]==310)
+            scoreA=scoreA+3;
+        if((player1[i])%100==14 || (player1[i])%100==11)
+            scoreA=scoreA+1;
+
+        if(player2[i]==202)
+            scoreB=scoreB+2;
+        if(player2[i]==310)
+            scoreB=scoreB+3;
+        if((player2[i])%100==14 || (player2[i])%100==11)
+            scoreB=scoreB+1;
+    }
+}
+
+int main(){
+    int deck1[52]; createDeck(deck1); shuffleDeck(deck1);
+    int player1[52]; createEmpityDeck(player1);
+    int hand1[4];
+    int player2[52]; createEmpityDeck(player2);
+    int hand2[4];
+    int ground[52]; createEmpityDeck(ground);
+
+    ptrA=player1;
+    ptrB=player2;
+
+    deal(deck1,ground);
+
+    for(int turn=1;turn<7;turn++){
+        if(turn==1)
+            printf("------ OYUN BASLADI ------");
+        printf("\n\n\n--%d. TUR--\n",turn);
+        printf("\nYerdeki En ustteki kart  : ");
+        printCard(getTop(ground));
+        deal(deck1,hand1);
+        deal(deck1,hand2);
+
+        for(int i=0;i<4;i++){
+            printf("\n\nElinizdeki Kartlar  :   ");
+            for(int i=0;i<4;i++){
+                printf("%d- ",i+1);
+                printCard(hand1[i]);
+            }
+            chooseCard(hand1,ground,player1);
+            printf("\nEn Ustteki Kart :");
+            printCard(getTop(ground));
+            chooseComCard(hand2,ground,player2);
+            printf("\n\nEn Ustteki Kart :");
+            printCard(getTop(ground));
+        }
+        if(turn==6){
+            if(lastCaptured==0)
+                captureRemainedCards(player1,ground);
+            if(lastCaptured==1)
+                captureRemainedCards(player2,ground);
+        }
+        printf("\n\nOyuncu'nun Topladigi Kartlar  : ");
+        printCards(player1,52);
+        printf("\n\nBilgisayar'in Topladigi Kartlar  : ");
+        printCards(player2,52);
+    }
+    scoreCalculator(player1,player2);
+    printf("\nOYUNCUNUN SKORU  : %d",scoreA);
+    printf("\nBILGISAYARIN SKORU  : %d",scoreB);
+    if(scoreA>scoreB)
+        printf("\n---  OYUNCU KAZANDI  ---");
+    else if(scoreA<scoreB)
+        printf("\n---  BILGISAYAR KAZANDI  ---");
+    else
+        printf("\n---  BERABERE  ---");
+}
